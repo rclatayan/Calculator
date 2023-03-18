7877b502ece1cab51e2875fdfd65dc786e14babf
Vincent <pazvincent6@gmail.com>
2023-03-17 19:53:32 -0700

    Initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Add(View view) {
+        EditText et1 = (EditText) findViewById(R.id.editTextNumber);
+        EditText et2 = (EditText) findViewById(R.id.editTextNumber2);
+        EditText et3 = (EditText) findViewById(R.id.editTextNumber3);
+
+        int n1 = Integer.parseInt(et1.getText().toString());
+        int n2 = Integer.parseInt(et1.getText().toString());
+        int result = n1 + n2;
+
+        et3.setText("Total Value " + result);
+    }
+
+    public void Subtract(View view) {
+        EditText et1 = (EditText) findViewById(R.id.editTextNumber);
+        EditText et2 = (EditText) findViewById(R.id.editTextNumber2);
+        EditText et3 = (EditText) findViewById(R.id.editTextNumber3);
+
+        int n1 = Integer.parseInt(et1.getText().toString());
+        int n2 = Integer.parseInt(et1.getText().toString());
+        int result = n1 - n2;
+
+        et3.setText("Subtract Value " + result);
+    }
+}
